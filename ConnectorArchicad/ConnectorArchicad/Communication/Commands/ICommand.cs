60f4c0c5d8bb9ced273452887dda9bfb0c9c897a
Istvan David <istvan.david@gmail.com>
2022-07-17 21:27:14 +0200

    Unneded imports

--- ConnectorArchicad/ConnectorArchicad/Communication/Commands/ICommand.cs
+++ ConnectorArchicad/ConnectorArchicad/Communication/Commands/ICommand.cs
@@ -1,10 +1,4 @@
 ﻿using System.Threading.Tasks;
-using System.Runtime.Serialization;
-using Newtonsoft.Json;
-using System.Text;
-using System;
-using System.IO.MemoryMappedFiles;
-using System.Threading;
 
 namespace Archicad.Communication.Commands
 {
